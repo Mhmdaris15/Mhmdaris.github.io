28066cb350d8e652ccc05635b43bfae3a9bc7386
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2022-02-26 11:06:04 +0700

    Make web responsive

--- Restaurant-Website/script.js
+++ Restaurant-Website/script.js
@@ -1,1 +1,26 @@
-console.log();
+let displayNav = false;
+let displayAbout = true;
+let aboutFrance = document.querySelector(".about-content");
+let body = document.querySelector("body");
+
+function dropDownAbout() { 
+    if (displayAbout){
+        aboutFrance.style.display = "none";
+        displayAbout = false;
+    } else {
+        aboutFrance.style.display = "flex";
+        displayAbout = true;
+    }
+ }
+
+function dropDownMenu(){
+    navbar = document.querySelector(".nav-bar-dropdown");
+    
+    if (!displayNav){
+        navbar.style.display = "flex";
+        displayNav = true;
+    } else {
+        navbar.style.display = "none";
+        displayNav = false;
+    }
+}
